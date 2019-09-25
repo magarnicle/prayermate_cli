4110cd0060e36b563a5fb688d3eaf378f6bf3e1f
Matt Rademaker <matthew@acctv.com.au>
2019-09-25 13:21:26 +1000

    appending is possible

---
+++ bin/append_card_text.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+category_idx=$(bin/get_category_idx.sh "$1" "$5")
+subject_idx=$(bin/get_subject_idx.sh $category_idx "$2" "$5")
+cleaned_text=$(echo "$4" | sed -e 's/^"//' -e 's/"$//' -e 's/\\n//g' -e 's/\\t/	/g')
+jq '.Categories['"$category_idx"'].subjects['"$subject_idx"'].cards['"$3"'].text += "\n'"$cleaned_text"'"' $5
+
