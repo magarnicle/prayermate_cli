5099754c7f3150a541d4d3f55acbd9bddcae608c
Matt Rademaker <matthew@acctv.com.au>
2019-12-02 12:48:18 +1000

    prepend works better

--- bin/prepend_card_text.sh
+++ bin/prepend_card_text.sh
@@ -3,6 +3,6 @@ category_idx=$(bin/get_category_idx.sh "$1" "$5")
 subject_idx=$(bin/get_subject_idx.sh $category_idx "$2" "$5")
 cleaned_text=$(echo "$4" | sed -e 's/^"//' -e 's/"$//' -e 's/\\n//g' -e 's/\\t/	/g')
 current_text=$(bin/get_card_text.sh "$1" "$2" "$3" "$5")
-bin/set_card_text.sh "$1" "$2" "$3" "$cleaned_text" "$5"
-bin/append_card_text.sh "$1" "$2" "$3" "$current_text" "$5"
+combined="$cleaned_text""$current_text"
+bin/set_card_text.sh "$1" "$2" "$3" "$combined" "$5"
 
